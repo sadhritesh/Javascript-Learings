4ec234945478d0f548391fe68131cd39a1f2cd9f
sadhritesh <108236576+sadhritesh@users.noreply.github.com>
2024-01-08 09:18:40 +0000

    Number and Math

---
+++ 01_basics/Lecture_10.js
@@ -0,0 +1,37 @@
+/*✅Numbers :- 
+
+let a = 12; 
+let b = new Number(13);
+
+ 
+
+toExponential : ƒ toExponential()
+toFixed : ƒ toFixed()
+toLocaleString : ƒ toLocaleString()
+toPrecision : ƒ toPrecision()
+toString : ƒ toString()
+valueOf :  ƒ valueOf()
+*/
+
+/*✅Maths :- 
+*/
+
+let a = Math.PI;
+console.log(a) //3.141592653589793
+
+let b = Math.SQRT2;
+console.log(b); //1.4142135623730951
+
+let c = 0.2;
+console.log(Math.ceil(c)); //1 
+
+console.log(Math.floor(Math.random()*10));
+
+
+//If we want values in certain range :
+
+let min = 100 ;
+let max = 200 ;
+
+console.log(Math.floor(Math.random() * (max - min + 1))+min);
+
